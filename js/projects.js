f38ce51b0ea5aef55c0f5237eb7ff9cfd7bbeb1a
l0tkaa <106283020+l0tkaa@users.noreply.github.com>
2025-02-15 16:40:30 -0600

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", loadProjects);
 
 function loadProjects() {
     const projects = [
+        {     title: "Snake Game",
+            description: "Classic Snake game built with Python and Pygame. The player controls a snake that moves around the screen, eats food to grow, and avoids crashing into itself.",
+            tech: "Python, Pygame",
+            github: "https://github.com/l0tkaa/Snake_Game",
+            demo: "<img src="https://github.com/user-attachments/assets/816ecac9-2867-4a53-9c88-b6cd9d2a2c13" width="350">"
+        },
         {
             title: "AI Stock Predictor",
             description: "A machine learning model that predicts stock prices based on historical data.",
